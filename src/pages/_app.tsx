9699954ec470b35a4c93f26d5b40b60605ea7ab6
marcosfa1 <filipe_araujo21@hotmail.com>
2023-01-15 23:22:12 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from "../styles/pages/app";
+
+import Image from 'next/image';
+
+globalStyles()
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
+
+export default MyApp
+
